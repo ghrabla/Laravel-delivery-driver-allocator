0cb2b2201990b29083c5d09755edc42fb2f2846a
ghrabla <kamalghrabla@gmail.com>
2025-06-19 23:59:04 +0100

    logs

--- app/Http/Controllers/AssignDriverController.php
+++ app/Http/Controllers/AssignDriverController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Driver;
 use App\Services\DriverService;
 use App\Http\Requests\AssignDriverRequest;
+use App\Models\Restaurant;
 use App\Services\DispatcherService;
 use App\Services\RestaurantService;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class AssignDriverController extends Controller
 {
@@ -41,12 +43,21 @@ class AssignDriverController extends Controller
                 $restaurant
             );
 
+            Log::info('Driver assigned successfully', [
+                Driver::ID => $driver[Driver::ID],
+                Restaurant::ID => $restaurant[Restaurant::ID],
+            ]);
 
             return response()->json([
                 'message' => 'Driver assigned successfully',
                 'data' => $driver
             ], 200);
         } catch (Exception $e) {
+            Log::error('Error assigning driver', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+
             return response()->json([
                 'message' => 'An error occurred while assigning a driver.',
                 'error' => $e->getMessage()
